613885018fd0fba362c43501be14e181c31b6dde
João Paulo Bastos <joao.bastos@mav.com.br>
2018-08-13 08:26:50 -0300

    Update volume control logic and add volume block in i3blocks.conf

--- volume_mute.sh
+++ volume_mute.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sink=`pactl list short sinks | grep RUNNING | cut -f1`                                                                                                                    
-notify-send "Volume Mute Toggle" -t 500
+sink=`pactl list short sinks | grep RUNNING | cut -f1`
+notify-send "Volume Mute Toggle" -t 0.1
 /usr/bin/pactl set-sink-mute $sink toggle
 
