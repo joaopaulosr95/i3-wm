a5651bf9a6cfddd006e51fffec8d856d38c84894
João Paulo Bastos <joao.bastos@mav.com.br>
2018-09-05 08:47:00 -0300

    General refactor for window placement

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-if [[ $(cat /etc/os-release | grep VERSION_ID | awk -F "\"" '{print $2}') != '18.04' ]]; then 
-	echo "This script is built for Ubuntu release 18.04 only. If you want to go on anyway, open the bootstrap script and uncomment this condition"
-	exit 1
-fi
-
 sudo apt update
 sudo apt upgrade -qy
-sudo apt install -qy i3-wm i3lock i3blocks scrot rofi nitrogen dunst feh lxappearance numlockx
+sudo apt install -qy \
+	i3-wm \
+	i3lock \
+	i3blocks \
+	scrot \
+	rofi \
+	nitrogen \
+	dunst \
+	feh \
+	lxappearance \
+	numlockx
 cp .Xresources ~
+sudo ln -sf . ~/.i3
